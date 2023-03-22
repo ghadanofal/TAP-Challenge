d40d0c5a645d09cc1cd47b3b2e9d9504f49c58ad
ghadanofal <181102@ppu.edu.ps>
2023-03-22 18:54:28 +0200

    first commit

---
+++ challenge2/Challenge2.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+let obj ={};
+let count=0;
+let max=0;
+
+for(let i of nums){
+    if(!obj[i])
+    {
+        obj[i]=1;
+
+    }else{
+        obj[i]++;
+}
+} 
+  //console.log(obj);
+for(let j in obj)
+{
+    if(obj[j]>count)
+{ count = obj[j];
+    max =j ;
+}
+}
+return max;
+};
